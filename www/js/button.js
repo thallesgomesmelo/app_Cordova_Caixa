054f3d1b101625bcfbd89da9c7a5209d61996fda
unknown <thallesgomesmello@gmail.com>
2021-11-24 18:37:29 -0300

    Implementou função de ler dados.

--- www/js/button.js
+++ www/js/button.js
@@ -3,28 +3,19 @@ let buttonEnviar = document.getElementById('send')
 let buttonLer = document.getElementById('ready')
 
 ligarBomba = () => {
-    let bomba = ""
-    bomba += '\n'
-
-    alert("Função botao Ligar")
+    let bomba = "bb"
     bluetoothSerial.write(bomba,null, () => {alert('Erro: Comando de ligar bomba falhou')})
 }
 buttonLigar.addEventListener('click',ligarBomba)
 
 enviarDados = () => {
-    let enviar = ""
-    enviar += '\n'
-
-    alert("Função botao Enviar")
+    let enviar = "ev"
     bluetoothSerial.write(enviar,null, () => {alert('Erro: Comando de enviar dados falhou')})
 }
 buttonEnviar.addEventListener('click',enviarDados)
 
 lerDados = () => {
-    let ler = ""
-    ler += '\n'
-
-    alert("Função botao Ler")
+    let ler = "lr"
     bluetoothSerial.write(ler,null, () => {alert('Erro: Comando de ler dados falhou')})
 }
 buttonLer.addEventListener('click',lerDados)
--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,11 @@
 let address = ""
+let getData = []
 let list = document.getElementById("listDevices")
+let inputInstalada = document.getElementById('alturaInstalada')
+let inputMax = document.getElementById('alturaMax')
+let inputMin = document.getElementById('alturaMin')
+let inputAtual = document.getElementById('alturaAtual')
+
 
 refresh = () => {
     address = ""
@@ -9,6 +15,13 @@ refresh = () => {
 
 clean = () => {
     document.getElementById('listDevices').innerHTML = ' '
+}
+
+inputClean = () => {
+    inputInstalada.value = ''
+    inputMax.value = ''
+    inputMin.value = ''
+    inputAtual.value = ''
 }
 
 // cleanTerminal = () => {
@@ -88,8 +101,7 @@ const App = {
 
     openTerminal() {
         document.querySelector('.terminal').classList.add('active')
-        // cleanTerminal()
-
+        inputClean()
         bluetoothSerial.subscribe('\n', App.handleData, error => {alert("Erro:\n" + error)})
     },
 
@@ -106,11 +118,23 @@ const App = {
        
         if (isIncoming) {
             //resposta do dispositivo
-            alert("Conectado" + data)
+            if(/ev/.test(data)){
+                //Pegando valores e separando, depois adiciona a um array
+                let guarda = data.slice(3)
+                getData = guarda.split(",")
+                
+                inputClean() //Limpo os campos input depois adiciono valores a eles
+
+                //Adiciomando valores nos campos input
+                inputInstalada.value = getData[0]
+                inputMax.value = getData[1]
+                inputMin.value = getData[2]
+                inputAtual.value = getData[3]
+            }
         } else {
             //dados que usuario envia
+            alert("estou enviando: " + data)
         }
-        dataContainer.append(data)
     }
 }
 
